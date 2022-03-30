7275b6a97fd5b6244a8cb6f353696fc51020c35d
大川峻 <shun@ookawashunnoMacBook-Air.local>
2022-03-30 14:59:20 +0900

    設定されたストレージにPostしたファイルを設置する処理を記載

--- app/Http/Controllers/FilePostController.php
+++ app/Http/Controllers/FilePostController.php
@@ -11,7 +11,22 @@ class FilePostController extends Controller
     {
         $file = $request['file'];
 
-        Storage::disk('s3')->putFile('/', $file, 'public');
-        return Storage::putFile('/tmp', $file, 'public');
+//        if (Storage::disk('s3')->exists('8B8BE13C-7504-47A9-B83E-9AC5E7DD4BA7.JPEG')) {
+//        if (Storage::disk('s3')->exists('s38B8BE13C-7504-47A9-B83E-9AC5E7DD4BA7.JPEG')) {
+//            //return Storage::url('s38B8BE13C-7504-47A9-B83E-9AC5E7DD4BA7.JPEG');
+//        } else {
+//            return 'NG';
+//        }
+
+        $result['filepath'] = Storage::putFile('/', $file/* , 'public' */);
+        return $result;
+//        return Storage::exists('s38B8BE13C-7504-47A9-B83E-9AC5E7DD4BA7.JPEG');
+/*         $result['filepath'] = Storage::putFile('/', $file, 'public');
+        if ($result['filepath']) {
+            return 'NG';
+        }
+        return 'OK'; */
+//        return Storage::disk('s3')->putFile('/', $file, 'public');
+//        return Storage::putFile('/tmp', $file, 'public');
     }
 }
